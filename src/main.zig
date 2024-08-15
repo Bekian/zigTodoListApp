0550a160b346fafdeada97b373bd9f26dbc36358
Bekian <Bekian.Cat@gmail.com>
2024-08-14 21:07:37 -0500

    working on time

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const time = std.time;
 
 // should be able to take the following commands:
 // add <task string arg>
@@ -49,10 +50,21 @@ pub fn addTask(file: []const u8) !void {
     defer currentfile.close();
     // jump to the end of the file
     try currentfile.seekFromEnd(0);
-    // create a buffered writer for the current file
+    // create a buffered writer for the current file (to write to the file)
     var buf_writer = std.io.bufferedWriter(currentfile.writer());
     // get the generic writer from buffered writer (idk why we need to do this)
     var out_stream = buf_writer.writer();
+
+    // get a timestamp and make it human readable
+    // get unix timestamp in seconds
+    const creationTimeStamp = time.timestamp();
+    const eSeconds = time.epoch.EpochSeconds{ .secs = @intCast(creationTimeStamp) };
+    // convert the timestamp to a readable format
+    const eDayCreationTime = time.epoch.EpochSeconds.getEpochDay(eSeconds).calculateYearDay();
+    const creationMonth = findMonth(eDayCreationTime.calculateMonthDay().month);
+    _ = creationMonth; // autofix
+    // TODO: also get the dayindex from eDayCreationTime.calculateMonthDay().month.day_index and year from eDayCreationtime.year
+
     // create a stdin reader and writer to read and write to and from the terminal
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
@@ -66,10 +78,14 @@ pub fn addTask(file: []const u8) !void {
         if (user_input.len >= 1) {
             // write a validation message to the user with their task
             try stdout.print("Task: {s}!\n", .{user_input});
+
             // trim any trailing newline
             const trimmed_output = std.mem.trimRight(u8, user_input, "\r\n");
+            var newLineBuffer: [150]u8 = undefined; // trying this length and i'll adjust the size if necessary
+            const newTaskLine = try std.fmt.bufPrint(&newLineBuffer, "{s}\t\t{any}!", .{ trimmed_output, eDayCreationTime.calculateMonthDay() });
+
             // write the trimmed output to the file
-            try out_stream.print("\n{s}", .{trimmed_output});
+            try out_stream.print("\n{s}", .{newTaskLine});
         } else {
             // if the user didnt provide a task, then inform the user of this.
             std.debug.print("No task provided, try again.", .{});
@@ -77,6 +93,11 @@ pub fn addTask(file: []const u8) !void {
     }
     // flush the writer (the writer is like a cache that must be cleared)
     try buf_writer.flush();
+}
+
+pub fn findMonth(month: std.time.epoch.Month) ![]u8 {
+    _ = month; // autofix
+    // switchcase to return the []u8 array of a month
 }
 
 pub fn main() !void {
