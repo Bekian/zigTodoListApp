d697e4e72d894980f7b67c622ff8e978a6636a90
Bekian <Bekian.Cat@gmail.com>
2024-08-27 22:31:28 -0500

    feat(app): added repl and arg branching
    added logic to test the basic logic of determining if the user used the executable in arg mode or repl mode, modified various comments, moved checkBool and convertTimestamp to be more organized with how the functions are laid out, added the use of convertTimestamp to the listTasks function so it shows the task in a user readable format.

--- src/main.zig
+++ src/main.zig
@@ -10,51 +10,13 @@ const time = std.time;
 
 const Task = struct { ID: u8, TaskDescription: []const u8, Creation: i64, Completed: bool };
 
-pub fn checkBool(inputString: []const u8) !bool {
-    // Should NOT equal anything else besides these 2 values
-    if (std.mem.eql(u8, inputString, "True")) {
-        return true;
-    } else if (std.mem.eql(u8, inputString, "False")) {
-        return false;
-    } else {
-        // if the value is not one of these then theres a parsing error
-        // tell user then crash
-        std.debug.print("\n PROCCESSING ERROR: condition value invalid. Verify your source file is the correct structure.", .{});
-        return error.ConditionValueInvalid;
-    }
-}
-
-// TODO: will be refactored or removed
-pub fn printWord(word: []const u8) !void {
-    if (word.len >= 20) {
-        std.debug.print("{s}\t", .{word});
-    } else if (word.len > 14) {
-        std.debug.print("{s}\t", .{word});
-    } else if (word.len > 9) {
-        std.debug.print("{s}\t\t", .{word});
-    } else {
-        std.debug.print("{s}\t\t\t", .{word});
-    }
-}
-
-// converts a time.timestamp() i64 to a human readable string
-pub fn convertTimestamp(timestamp: i64) ![]const u8 {
-    const eSeconds = time.epoch.EpochSeconds{ .secs = @intCast(timestamp) };
-    // convert the timestamp to a readable format
-    const eDayCreationTime = time.epoch.EpochSeconds.getEpochDay(eSeconds).calculateYearDay();
-    const creationMonth = try findMonth(eDayCreationTime.calculateMonthDay().month);
-    const creationDay = eDayCreationTime.calculateMonthDay().day_index;
-    const creationYear = eDayCreationTime.year;
-    var formattingBuffer: [20]u8 = undefined;
-    return try std.fmt.bufPrint(&formattingBuffer, "{s} {d} {d}", .{ creationMonth, creationDay, creationYear });
-}
-
 // TODO: Review
 // lists all the current tasks in the task list
 pub fn listTasks(taskList: std.ArrayList(Task)) !void {
     std.debug.print("Tasks:\n", .{});
     for (taskList.items) |task| {
-        std.debug.print("ID: {}, Description: {s}, Creation: {}, Completed: {}\n", .{ task.ID, task.TaskDescription, task.Creation, task.Completed });
+        const formattedCreationDate = try convertTimestamp(task.Creation);
+        std.debug.print("ID: {}, Description: {s}, Creation: {s}, Completed: {}\n", .{ task.ID, task.TaskDescription, formattedCreationDate, task.Completed });
     }
 }
 
@@ -75,6 +37,7 @@ pub fn addTask(allocator: std.mem.Allocator, taskList: *std.ArrayList(Task), new
     try taskList.append(newTask);
 }
 
+// TODO: Review if necessary
 // switchcase to return the string name of a month
 pub fn findMonth(month: std.time.epoch.Month) ![]const u8 {
     switch (month) {
@@ -123,6 +86,33 @@ pub fn invalidCommand() void {}
 // Gets the last ID in the file as a string
 pub fn getLastId(taskList: std.ArrayList(Task)) !u8 {
     return taskList.getLast().ID;
+}
+
+// turns a string `True` or `False` into a bool
+pub fn checkBool(inputString: []const u8) !bool {
+    // Should NOT equal anything else besides these 2 values
+    if (std.mem.eql(u8, inputString, "True")) {
+        return true;
+    } else if (std.mem.eql(u8, inputString, "False")) {
+        return false;
+    } else {
+        // if the value is not one of these then theres a parsing error
+        // tell user then crash
+        std.debug.print("\n PROCCESSING ERROR: condition value invalid. Verify your source file is the correct structure.", .{});
+        return error.ConditionValueInvalid;
+    }
+}
+
+// converts a time.timestamp() i64 to a human readable string
+pub fn convertTimestamp(timestamp: i64) ![]const u8 {
+    const eSeconds = time.epoch.EpochSeconds{ .secs = @intCast(timestamp) };
+    // convert the timestamp to a readable format
+    const eDayCreationTime = time.epoch.EpochSeconds.getEpochDay(eSeconds).calculateYearDay();
+    const creationMonth = try findMonth(eDayCreationTime.calculateMonthDay().month);
+    const creationDay = eDayCreationTime.calculateMonthDay().day_index;
+    const creationYear = eDayCreationTime.year;
+    var formattingBuffer: [20]u8 = undefined;
+    return try std.fmt.bufPrint(&formattingBuffer, "{s} {d} {d}", .{ creationMonth, creationDay, creationYear });
 }
 
 // TODO: Look for optimizations
@@ -179,11 +169,12 @@ pub fn main() !void {
     // first open the file √
     // read the items and translate them into task structs √
     // call the getLastId function or equivalent √
-    // determine if the user used a repl or a command
+    // determine if the user used a repl or a command √
+
     // open csv with the mode set to read_write
     const currentfile = try std.fs.cwd().openFile("src/data.csv", .{ .mode = .read_write });
-    // defer closing
     defer currentfile.close();
+    // get an allocator for resizing the tasklist
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
@@ -192,16 +183,26 @@ pub fn main() !void {
 
     // this only adds a task it doesnt save the new task to the file
     // try addTask(allocator, &taskList, "test");
-
-    try listTasks(taskList); // display all the tasks
+    // get last task ID number
     // const lastID = try getLastId(taskList);
+    // display all the tasks
+    try listTasks(taskList);
 
     // command: TODO: determine what command was entered
     // parse the argument if any, and compute the result
+    var args = std.process.args();
+    _ = args.skip(); // skip executable name
+    if (args.next()) |arg| {
+        // here process args
+        std.debug.print("arg print: {s}\n", .{arg});
+    } else {
+        // here run repl
+        std.debug.print("No args!\n", .{});
+    }
 
     // repl: TODO: begin loop
     // set exit condition, ex: `exit`, `quit` etc.
-    // send prompt, ex: `>>>` or `task >>>`
+    // send prompt, ex: `>>>`
     // use stdin and stdout and take input and write to terminal
     // process input command
     // if no command then tell user help and exit commands
@@ -210,11 +211,7 @@ pub fn main() !void {
     // TODO: finish doc'ing out the scaffold for how the app flow should work and execute
     // try addTask("src/data.csv");
     // try listTasks("src/data.csv");
-    var args = std.process.args();
-    _ = args.skip(); // Skip the program name
-    if (args.next() != null) {
-        std.debug.print("{any}", .{args.inner});
-    }
+
     // const list = std.flag.Int("list", .{ .help = "List the created tasks", .useage = "`list` - lists uncomplete tasks by default.\n`list --all` - lists all tasks." });
 
     // _ = try std.flag.parse(std.process.args());
